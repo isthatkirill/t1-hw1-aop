d285ba43ab10c9ce9320b3f19fccbb5862406db8
isthatkirill <kirill.emelyanoff2021@yandex.ru>
2024-05-01 17:48:29 +0300

    init: project initialized via spring initializr

---
+++ src/main/java/isthatkirill/hwoneaop/HwOneAopApplication.java
@@ -0,0 +1,13 @@
+package isthatkirill.hwoneaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwOneAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwOneAopApplication.class, args);
+	}
+
+}
